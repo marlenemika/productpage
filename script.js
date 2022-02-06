958d6fd34bb8e957ae147ceca1a52207a4ebf4df
Max Mika <max.mika@mhp.com>
2022-02-06 01:20:38 +0100

    * filled with content * moved legal texts

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ function changeColour()
 {
     document.getElementByClass("button").style.color="#968454";
 }
+
+function onContactButtonClicked() {
+    alert("Mail");
+    window.open('mailto:hi.emmics@gmail.com?subject=RSVP App');
+}
