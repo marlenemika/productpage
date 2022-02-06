12f4e45a4905ef9eb68b7e44242fd1b1ea3f4d29
Marlene Mika <marlene.mika@uni-ulm.de>
2022-02-06 20:21:50 +0100

    changes

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function changeColour()
     document.getElementByClass("button").style.color="#968454";
 }
 
-function onContactButtonClicked() {
+/*unction onContactButtonClicked() {
     alert("Mail");
     window.open('mailto:hi.emmics@gmail.com?subject=RSVP App');
-}
+}*/
