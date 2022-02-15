0979a4fa2a313142ed7c530e18742a9bf225e93b
Max Mika <max.mika@mhp.com>
2022-02-15 22:40:00 +0100

    * added live store URLs * uncommented store buttons

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ function changeColour()
 function onContactButtonClicked() {
     window.open('mailto:hi.emmics@gmail.com?subject=RSVP App');
 }
+
+function getPlayStoreUrl() {
+    return "https://play.google.com/store/apps/details?id=de.hochzeit.rvspapp";
+}
+
+function getAppStoreUrl() {
+    return "https://apps.apple.com/de/app/rsvp-wedding-app/id1597193395";
+}
